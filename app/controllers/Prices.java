9e741d0948345f720582ded31832df8085d686a7
Erik Billerby <erik.billerby@acando.com>
2013-05-03 15:59:02 +0200

    First charting...

--- app/controllers/Prices.java
+++ app/controllers/Prices.java
@@ -1,7 +1,14 @@
 package controllers;
 
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import models.Fish;
 import models.Quotation;
@@ -10,7 +17,16 @@ import org.joda.time.DateTime;
 
 import play.modules.paginate.ValuePaginator;
 import play.mvc.Controller;
+import utils.DateTypeAdapter;
 import utils.DateUtil;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 public class Prices extends Controller {
 	
@@ -35,10 +51,15 @@ public class Prices extends Controller {
 		render();
 	}
 	
-	public static void plot(String fishId){
+	
+	public static void getDataForSpecies(String fishId){
 		Fish fish = Fish.findById(new Long(fishId));
 		List<Quotation> quotations = Quotation.find("bySpecies", fish).fetch();
-		render();
+		Collections.sort(quotations);
+		DateTypeAdapter adapters = new DateTypeAdapter();
+		renderJSON(quotations, adapters);
+		
 	}
-
+	
+	
 }
